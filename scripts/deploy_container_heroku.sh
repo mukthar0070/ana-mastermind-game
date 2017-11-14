14d96527a65f7051bfe7a872309ec8c45cec26a4
Ana Cardoso <cardoso.anacp@gmail.com>
2017-11-13 22:55:43 -0200

    add script file

---
+++ scripts/deploy_container_heroku.sh
@@ -0,0 +1,3 @@
+docker build --rm=false -t registry.heroku.com/dev-ana-mastermind-game/web .;
+docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com;
+docker push registry.heroku.com/dev-ana-mastermind-game/web
