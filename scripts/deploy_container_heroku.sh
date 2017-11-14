753d89b405350accbddc386cc212026f251be4c7
Ana Cardoso <cardoso.anacp@gmail.com>
2017-11-13 23:08:53 -0200

    add heroku_app env variable

--- scripts/deploy_container_heroku.sh
+++ scripts/deploy_container_heroku.sh
@@ -1,3 +1,4 @@
+echo $HEROKU_APP
 docker build --rm=false -t registry.heroku.com/dev-ana-mastermind-game/web .;
 docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker push registry.heroku.com/dev-ana-mastermind-game/web
