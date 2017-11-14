07a0c1f75c151014585c5630ff35e49ff65a7219
Ana Cardoso <acardoso@thoughtworks.com>
2017-11-14 18:56:01 -0200

    build image once instead of build it on each stage #20

--- scripts/deploy_container_heroku.sh
+++ scripts/deploy_container_heroku.sh
@@ -1,3 +1,4 @@
-docker build --rm=false -t registry.heroku.com/$HEROKU_APP/web .;
+docker pull $DOCKER_USERNAME/ana-mastermind-game
+docker tag ana-mastermind-game $DOCKER_USERNAME/$HEROKU_APP
 docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker push registry.heroku.com/$HEROKU_APP/web
