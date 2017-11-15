266495bb77a0795d587a45a18ffcdce770f492e7
Ana Cardoso <acardoso@thoughtworks.com>
2017-11-15 10:30:04 -0200

    fix heroku container name in deploy script

--- scripts/deploy_container_heroku.sh
+++ scripts/deploy_container_heroku.sh
@@ -1,4 +1,4 @@
 docker pull $DOCKER_USERNAME/ana-mastermind-game
-docker tag $DOCKER_USERNAME/ana-mastermind-game $DOCKER_USERNAME/$HEROKU_APP
+docker tag $DOCKER_USERNAME/ana-mastermind-game registry.heroku.com/$HEROKU_APP/web
 docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker push registry.heroku.com/$HEROKU_APP/web
